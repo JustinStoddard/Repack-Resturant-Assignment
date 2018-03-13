57e541633a0fae367a071716ad31af7d706c54da
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-13 00:34:06 -0600

    New Updates

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Menu = () => (
-    <h1>Menu</h1>
-)
+class Menu extends React.Component {
+  state = { menu_items: [] }
+
+  componentDidMount() {
+    axios.get('/api/menu_items')
+      .then( res => this.setState({ menu_items: res.data }) )
+  }
+
+  render() {
+    const { menu_items } = this.state;
+     return(
+      <ul>
+        { menu_items.map( m =>
+            <li key={m.id}>
+              <Link to={`/menu_items/${m.id}`}>{m.name}</Link>
+            </li>
+          )
+        }
+      </ul>
+    ) 
+  }
+}
 
 export default Menu;
